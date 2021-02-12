4b6b6d306a6313296f3b74bc6f478ffbcea0d4ae
Daniel151505 <daniel150520011@gmail.com>
2021-02-12 16:57:49 -0500

    Creando Model Tarea

---
+++ Aplicacion-Tareas/src/app/models/tarea.models.ts
@@ -0,0 +1,11 @@
+export class tarea {
+    constructor(nombre:string, estado:boolean) {
+        this.nombre=nombre
+        this.estado=estado
+    }
+
+    nombre: string
+    estado: boolean
+    
+
+}
